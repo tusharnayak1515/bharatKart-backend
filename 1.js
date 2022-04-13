556013c4f4423555ca566c02c1b8b999cbd16c63
Tushar Ranjan Nayak <tusharnayak1515@gmail.com>
2022-04-13 18:25:53 +0530

    updated products json to be added

--- 1.js
+++ 1.js
@@ -1,5 +1,6 @@
 // {
 //     "name": "MI Official IPL Jersey",
+//     "category": "T-Shirts",
 //     "description": "This is the official IPL jersey of Mumbai Indians.",
 //     "image": "https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1647017777_7778235.jpg?format=webp&w=480&dpr=1.3",
 //     "price": 999,
@@ -7,6 +8,7 @@
 // },
 // {
 //     "name": "CSK Official IPL Jersey",
+//     "category": "T-Shirts",
 //     "description": "This is the official IPL jersey of Chennai Super Kings.",
 //     "image": "https://images.fancode.com/media/catalog/product/cache/c864083bf74ae97037fa23bc5340ae95/m/a/match-jersey-_front_efohmwybcdac7sxn.png",
 //     "price": 999,
@@ -14,6 +16,7 @@
 // },
 // {
 //     "name": "DC Official IPL Jersey",
+//     "category": "T-Shirts",
 //     "description": "This is the official IPL jersey of Delhi Capitals.",
 //     "image": "https://images.fancode.com/media/catalog/product/cache/c864083bf74ae97037fa23bc5340ae95/d/c/dcjs206-1_dwkqf6vlsce3bdk0.jpg",
 //     "price": 999,
@@ -21,6 +24,7 @@
 // },
 // {
 //     "name": "RCB Official IPL Jersey",
+//     "category": "T-Shirts",
 //     "description": "This is the official IPL jersey of Royal Challengers Bangalore.",
 //     "image": "https://images.fancode.com/media/catalog/product/cache/c864083bf74ae97037fa23bc5340ae95/7/7/773001-01_bumrytayewhom1ji.png",
 //     "price": 999,
@@ -28,6 +32,7 @@
 // },
 // {
 //     "name": "RR Official IPL Jersey",
+//     "category": "T-Shirts",
 //     "description": "This is the official IPL jersey of Rajasthan Royals.",
 //     "image": "https://cdn.shopify.com/s/files/1/0550/5577/7947/products/2022-3-15-18-39-12_product_3_1647349752859_1080x.jpg?v=1647349784",
 //     "price": 999,
@@ -35,6 +40,7 @@
 // },
 // {
 //     "name": "SRH Official IPL Jersey",
+//     "category": "T-Shirts",
 //     "description": "This is the official IPL jersey of Sunrisers Hyderabad.",
 //     "image": "https://images.fancode.com/media/catalog/product/cache/c864083bf74ae97037fa23bc5340ae95/s/h/shjs203-3_nh38sroafpknxhed.jpg",
 //     "price": 999,
@@ -42,6 +48,7 @@
 // },
 // {
 //     "name": "PK Official IPL Jersey",
+//     "category": "T-Shirts",
 //     "description": "This is the official IPL jersey of Punjab Kings.",
 //     "image": "https://images.fancode.com/media/catalog/product/cache/c864083bf74ae97037fa23bc5340ae95/k/p/kpjs205-1_24rgktjk5zyiiyve.jpg",
 //     "price": 999,
@@ -49,6 +56,7 @@
 // },
 // {
 //     "name": "KKR Official IPL Jersey",
+//     "category": "T-Shirts",
 //     "description": "This is the official IPL jersey of Kolkata Knight Riders.",
 //     "image": "https://images.fancode.com/media/catalog/product/cache/c864083bf74ae97037fa23bc5340ae95/k/r/krjs203-1_8vougfm9nl3d17ot.jpg",
 //     "price": 999,
@@ -56,6 +64,7 @@
 // },
 // {
 //     "name": "GT Official IPL Jersey",
+//     "category": "T-Shirts",
 //     "description": "This is the official IPL jersey of Kolkata Gujarat Titans.",
 //     "image": "https://webmerx.sgp1.cdn.digitaloceanspaces.com/ethnicflix/product_images/1648451255.shopping_(1).webp",
 //     "price": 999,
@@ -63,6 +72,7 @@
 // },
 // {
 //     "name": "LSG Official IPL Jersey",
+//     "category": "T-Shirts",
 //     "description": "This is the official IPL jersey of Lucknow Super Giants.",
 //     "image": "https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1648454232_9247036.jpg?format=webp&w=480&dpr=1.3",
 //     "price": 999,
